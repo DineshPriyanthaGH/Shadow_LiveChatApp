fbeff28596c01de0d05f29d634708b88d82a25a4
DineshPriyantha <priyanthadinesh2002@gmail.com>
2024-09-17 11:01:25 +0530

    Up:Search BAr

--- src/components/list/chatlist/Chatlist.jsx
+++ src/components/list/chatlist/Chatlist.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import "./Chatlist.css";
 
 const Chatlist = () => {
-    return <div>Chatlist</div>;
+    const [addMode,setAddMode]=useState(false)
+    return (
+        <div className="chatlist">
+            <div className="search">
+                <div className="searchBar">
+                    <img src="./search.png" alt="search" />
+                    <input type="text" placeholder="Search" />
+
+
+                </div>
+                <img src={addMode ? "./minus.png":"./plus.png"} alt="add"  className="add" onClick={()=> setAddMode((prev)=>!prev)} />
+            </div>
+        </div>
+    )
 };
 export default Chatlist;
