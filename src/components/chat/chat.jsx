4e129e1531af7150764acd8b249a2307358d82c8
DineshPriyantha <priyanthadinesh2002@gmail.com>
2024-09-17 15:59:14 +0530

    UP:chat top

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -2,7 +2,30 @@ import "./chat.css";
 
 const Chat = () => {
     return (
-      <div className='chat'>chat</div>
+      <div className='chat'>
+        <div className="top">
+            <div className="user">
+                <img src="./avatar.png" alt="user" />
+                <div className="texts">
+                    <span className="">User Name</span>
+                    <p>Hello, how can I help you?</p>
+
+                </div>
+            </div>
+            <div className="icons">
+                <img src="./phone.png" alt="phone" />
+                <img src="./video.png" alt="video" />
+                <img src="./info.png" alt="info" />
+
+            </div>
+        </div>
+        <div className="center"></div>
+        <div className="bottom">
+          
+            </div>
+        </div>
+      </div>
+
     )
   }
   
